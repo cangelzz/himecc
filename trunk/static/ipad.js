08d33b98bf26efe00aeb85bd983245f3ad51046a
cangelzz <cangelzz@4051cb7d-7cea-046c-04cc-0abe5444da1a>
2010-07-08 09:45:15 +0000

    go home

---
+++ trunk/static/ipad.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $('#progress').ajaxStart(function() {
+        $(this).show();
+    });
+
+    $('#progress').ajaxComplete(function() {
+        $(this).hide();
+        $("a").each(function(){
+            var url = this.href;
+            if (url.match(/javascript/)) return;
+            if (url.match(/(board|subject|post)/))
+            {
+                url = url.replace("board", "iboard").replace("subject","isubject","post","ipost");
+                this.href = "javascript:loadSmart('" + url + "')";
+            }
+        });
+     
+    });
+
+    $("#main").css("height", $(window).height()-$("#navboard").height());
+});
+
+function loadSmart(path) {
+    if (path.match(/iboard/)) loadBoard(path);
+    if (path.match(/isubject/)) loadSubject(path);
+    if (path.match(/ipost/)) loadPost(path);
+}
+
+
+function loadBoard(path)
+{
+    $('#divThreads').load(path)
+}
+
+function loadSubject(path)
+{
+    $('#divPosts').load(path)
+}
+
+function loadPost(path)
+{
+    $('#divPosts').load(path)
+}
+
+
+
