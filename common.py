7abf4ba5c0e955d729876cd4063925d382b7eebe
cangelzz <cangelzz@4051cb7d-7cea-046c-04cc-0abe5444da1a>
2010-07-20 07:09:38 +0000

    add some to robots

--- common.py
+++ common.py
@@ -15,6 +15,7 @@ tracking = """<script type="text/javascript">
 commonHeader = """<html><head>
 <link rel="Stylesheet" href="/static/my.css" media="screen" type="text/css" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="robots" content="noindex,nofollow,noarchive,nosnippet">
 <meta name="viewport" content="width=device-width, user-scalable=no">
 <meta name="viewport" content="initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />""" + tracking + """<script>
 function setOrientation() {
